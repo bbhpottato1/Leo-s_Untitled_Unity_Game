0554cc4986d7928c710fd5f548b28a4f45828d29
bbhpottato1 <bbhpottato14@gmail.com>
2025-08-19 00:38:34 -0400

    added movement system

--- Leo's Unititled game/Assets/Player/Player_Movement.cs
+++ Leo's Unititled game/Assets/Player/Player_Movement.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour{
 
+    public float speed;
     public Rigidbody2D body;
     void Start(){
         
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
 
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        
-        body.linearVelocity = new Vector2(xInput, yInput);
+
+        if (Mathf.Abs(xInput) > 0){
+            
+        }
+        if (Mathf.Abs(yInput) > 0){
+            
+        }
+
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+
+        body.linearVelocity = direction * speed;
     }
 }
