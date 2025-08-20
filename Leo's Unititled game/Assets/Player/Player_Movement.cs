10c65373bd57530fc07a51befd8b3d7a2c86c1ee
bbhpottato1 <bbhpottato14@gmail.com>
2025-08-19 22:22:36 -0400

    movement

--- Leo's Unititled game/Assets/Player/Player_Movement.cs
+++ Leo's Unititled game/Assets/Player/Player_Movement.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 
-public class Player_Movement : MonoBehaviour{
+public class Player_Movement : MonoBehaviour
+{
 
     public float speed;
+    public float drag;
     public Rigidbody2D body;
-    void Start(){
-        
+    public BoxCollider2D groundCheck;
+    public LayerMask groundMask;
+    public bool grounded;
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
@@ -15,15 +21,31 @@ public class Player_Movement : MonoBehaviour{
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(xInput) > 0){
-            
+        if (Mathf.Abs(xInput) > 0)
+        {
+            body.linearVelocity = new Vector2(xInput * speed, body.linearVelocity.y);
         }
-        if (Mathf.Abs(yInput) > 0){
-            
+        if (Mathf.Abs(yInput) > 0)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * speed);
         }
 
-        Vector2 direction = new Vector2(xInput, yInput).normalized;
+        //Vector2 direction = new Vector2(xInput, yInput).normalized;
 
-        body.linearVelocity = direction * speed;
+        //body.linearVelocity = direction * speed;
+    }
+    void FixedUpdate()
+    {
+        Checkground();
+        if (grounded)
+        {
+            body.linearVelocity *= drag;
+        }
+
+    }
+
+    void Checkground()
+    {
+        
     }
 }
